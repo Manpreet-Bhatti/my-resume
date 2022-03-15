41d4a9fd3efd8d696985239f243e6df3fd36fef9
Manpreet Bhatti <manpreet@bhatti.net>
2022-03-14 22:25:39 -0400

    Reverse Work data

--- src/sections/Work/index.jsx
+++ src/sections/Work/index.jsx
@@ -28,45 +28,49 @@ export default function Work() {
       render={(data) => (
         <section className="container">
           <h2 className={"title"}>Work</h2>
-          {data.allContentfulWork.edges.map((job, key) => {
-            return (
-              <div key={key}>
-                <div className={styles.header}>
-                  <h3 className={styles.title}>{job.node.company}</h3>
-                  <span className={styles.dates}>
-                    {job.node.start} {"\u2013"} {job.node.end}
-                  </span>
+          {data.allContentfulWork.edges
+            .slice(0)
+            .reverse()
+            .map((job, key) => {
+              return (
+                <div key={key}>
+                  <div className={styles.header}>
+                    <h3 className={styles.title}>{job.node.company}</h3>
+                    <span className={styles.dates}>
+                      {job.node.start} {"\u2013"} {job.node.end}
+                    </span>
+                  </div>
+                  <p className={styles.subtitle}>{job.node.position}</p>
+                  <div className={styles.content}>
+                    <ul>
+                      {job.node.childrenContentfulWorkDescriptionJsonNode.map(
+                        (content, i) => {
+                          return (
+                            <li key={i}>
+                              <p>
+                                {content.content}{" "}
+                                {content.link !== null &&
+                                  content.label !== null && (
+                                    <a
+                                      aria-label={content.label}
+                                      rel="noreferrer noopenner"
+                                      className={styles.link}
+                                      target="_blank"
+                                      href={content.link}
+                                    >
+                                      <FiGithub className={styles.icon} />
+                                    </a>
+                                  )}
+                              </p>
+                            </li>
+                          );
+                        },
+                      )}
+                    </ul>
+                  </div>
                 </div>
-                <p className={styles.subtitle}>{job.node.position}</p>
-                <div className={styles.content}>
-                  <ul>
-                    {job.node.childrenContentfulWorkDescriptionJsonNode.map(
-                      (content, i) => {
-                        return (
-                          <li key={i}>
-                            <p>
-                              {content.content}{" "}
-                              {content.link !== null && content.label !== null && (
-                                <a
-                                  aria-label={content.label}
-                                  rel="noreferrer noopenner"
-                                  className={styles.link}
-                                  target="_blank"
-                                  href={content.link}
-                                >
-                                  <FiGithub className={styles.icon} />
-                                </a>
-                              )}
-                            </p>
-                          </li>
-                        );
-                      },
-                    )}
-                  </ul>
-                </div>
-              </div>
-            );
-          })}
+              );
+            })}
         </section>
       )}
     />
