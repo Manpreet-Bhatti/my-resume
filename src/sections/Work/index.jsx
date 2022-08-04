570062a0f6775dd1a1fae7adf8f7ef92afc735c4
Manpreet Bhatti <manpreet@bhatti.net>
2022-08-04 17:53:03 -0400

    Update work section with new content model

--- src/sections/Work/index.jsx
+++ src/sections/Work/index.jsx
@@ -8,17 +8,15 @@ export default function Work() {
     <StaticQuery
       query={graphql`
         query WorkQuery {
-          allContentfulWork {
-            edges {
-              node {
-                start(formatString: "MMM DD YYYY")
-                end(formatString: "MMM DD YYYY")
-                company
-                home
-                position
-                description {
-                  content
-                }
+          contentfulListOfWork {
+            work {
+              start(formatString: "MMM DD YYYY")
+              end(formatString: "MMM DD YYYY")
+              company
+              home
+              position
+              description {
+                content
               }
             }
           }
@@ -27,66 +25,54 @@ export default function Work() {
       render={(data) => (
         <section className="container">
           <h2 className={"title"}>Work</h2>
-          {data.allContentfulWork.edges
-            .slice(0)
-            .reverse()
-            .map((job, key) => {
-              return (
-                <div key={key}>
-                  <div className={styles.header}>
-                    <h3 className={styles.title}>
-                      {job.node.company}&nbsp;&nbsp;
-                      {job.node.home && (
-                        <a
-                          href={job.node.home}
-                          target="_blank"
-                          rel="noreferrer"
-                        >
-                          <FiLink className={styles.icon} />
-                        </a>
-                      )}
-                    </h3>
-                    <span className={styles.dates}>
-                      {job.node.start.split(" ")[0] +
-                        " " +
-                        job.node.start.split(" ")[2]}{" "}
-                      {"\u2013"}{" "}
-                      {job.node.start.split(" ")[1] ===
-                      job.node.end.split(" ")[1]
-                        ? "Present"
-                        : job.node.end.split(" ")[0] +
-                          " " +
-                          job.node.end.split(" ")[2]}
-                    </span>
-                  </div>
-                  <p className={styles.subtitle}>{job.node.position}</p>
-                  <div className={styles.content}>
-                    <ul>
-                      {job.node.description.map((content, i) => {
-                        return (
-                          <li key={i}>
-                            <p>
-                              {content.content}{" "}
-                              {content.link && content.label && (
-                                <a
-                                  aria-label={content.label}
-                                  rel="noreferrer noopenner"
-                                  className={styles.link}
-                                  target="_blank"
-                                  href={content.link}
-                                >
-                                  <FiGithub className={styles.icon} />
-                                </a>
-                              )}
-                            </p>
-                          </li>
-                        );
-                      })}
-                    </ul>
-                  </div>
+          {data.contentfulListOfWork.work.map((job, key) => {
+            return (
+              <div key={key}>
+                <div className={styles.header}>
+                  <h3 className={styles.title}>
+                    {job.company}&nbsp;&nbsp;
+                    {job.home && (
+                      <a href={job.home} target="_blank" rel="noreferrer">
+                        <FiLink className={styles.icon} />
+                      </a>
+                    )}
+                  </h3>
+                  <span className={styles.dates}>
+                    {job.start.split(" ")[0] + " " + job.start.split(" ")[2]}{" "}
+                    {"\u2013"}{" "}
+                    {job.start.split(" ")[1] === job.end.split(" ")[1]
+                      ? "Present"
+                      : job.end.split(" ")[0] + " " + job.end.split(" ")[2]}
+                  </span>
                 </div>
-              );
-            })}
+                <p className={styles.subtitle}>{job.position}</p>
+                <div className={styles.content}>
+                  <ul>
+                    {job.description.map((content, i) => {
+                      return (
+                        <li key={i}>
+                          <p>
+                            {content.content}{" "}
+                            {content.link && content.label && (
+                              <a
+                                aria-label={content.label}
+                                rel="noreferrer noopenner"
+                                className={styles.link}
+                                target="_blank"
+                                href={content.link}
+                              >
+                                <FiGithub className={styles.icon} />
+                              </a>
+                            )}
+                          </p>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                </div>
+              </div>
+            );
+          })}
         </section>
       )}
     />
