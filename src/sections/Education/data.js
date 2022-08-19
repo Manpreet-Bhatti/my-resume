705ce1b701aa789b71c56332e0251a54c36c9195
Manpreet Bhatti <manpreet@bhatti.net>
2022-08-19 19:48:30 -0400

    Update volunteer experience

--- src/sections/Education/data.js
+++ src/sections/Education/data.js
@@ -1,13 +1,11 @@
 export const EduData = {
   title: "University of Toronto",
   start: 2018,
-  end: "Current",
+  end: "Present",
   degree: "Honours Bachelors of Science, Computer Science",
   content: [
     "Hack The Valley",
     "Association of Mathematics and Computer Science Students",
     "Computer Science Enrichment Club",
-    "Intramural Basketball",
-    "Intramural Cricket",
   ],
 };
