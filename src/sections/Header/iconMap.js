d621d295156fd2d024eb108a4eb96e9b112d3d0a
Manpreet Bhatti <manpreet@bhatti.net>
2022-10-20 00:14:47 -0400

    Assign object to variable before exporting

--- src/sections/Header/iconMap.js
+++ src/sections/Header/iconMap.js
@@ -1,6 +1,7 @@
-export default {
+const icons = {
   email: require("react-icons/fi").FiMail,
   website: require("react-icons/fa").FaReact,
   linkedin: require("react-icons/ri").RiLinkedinLine,
   github: require("react-icons/fi").FiGithub,
 };
+export default icons;
