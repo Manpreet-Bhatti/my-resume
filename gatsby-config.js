1f78a97c1deb598954690ee027d65c8a71221459
Manpreet Bhatti <manpreet@bhatti.net>
2022-10-19 23:58:55 -0400

    Update blurb to account for previous CM position

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 module.exports = {
   siteMetadata: {
     title: "my-resume",
-    blurb: "Development Intern @ Critical Mass",
+    blurb: "Previously @ Critical Mass",
     author: "Manpreet Bhatti",
     description,
   },
